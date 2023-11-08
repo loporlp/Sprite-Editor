c401f82d041d40e3934235a7d6c6c8bdd71ae09d
Mason Sansom <33166896+loporlp@users.noreply.github.com>
2023-11-08 11:19:03 -0700

    Added Button Widget to QFrame

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPushButton>
+#include <QImage>
+#include <QLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPushButton *btn = new QPushButton();
+    auto layout = new QVBoxLayout();
+    layout->addWidget(btn);
+    ui->canvas->setLayout(layout);
+
+    ui->canvas->setLineWidth(3);
 
 }
 
