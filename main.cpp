71cd662338da779c18e19207fd105c1f0b5607b5
David Cosby <davjcosby@gmail.com>
2023-11-08 12:40:19 -0700

    Construct model in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
+#include "model.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    Model m;
     MainWindow w;
     w.show();
     return a.exec();
