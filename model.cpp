63770a233738e4a1af2f213b2347afad1aa2fa99
David Cosby <davjcosby@gmail.com>
2023-11-08 11:31:07 -0700

    implemented frame manipulation methods

--- model.cpp
+++ model.cpp
@@ -1,7 +1,38 @@
 #include "model.h"
+#include <QObject>
 
 Model::Model(QObject *parent)
     : QObject{parent}
-{
+{}
 
+uint Model::numFrames()
+{
+    return frames.size();
+}
+
+QImage &Model::getFrame(uint index)
+{
+    assert(frames.size() > index);
+    return frames.at(index);
+}
+
+void Model::pushFrame(QImage frame)
+{
+    frames.push_back(frame);
+}
+
+void Model::insertFrame(QImage frame, uint index)
+{
+    frames.insert(frames.begin() + index, frame);
+}
+
+void Model::deleteFrame(uint index)
+{
+    assert(frames.size() > index);
+    frames.erase(frames.begin() + index);
+}
+
+void Model::popFrame()
+{
+    frames.pop_back();
 }
--- model.h
+++ model.h
@@ -14,14 +14,17 @@ public:
     /// returns the number of frames in our current animation
     uint numFrames();
 
+    /// returns a reference to the frame at index
+    QImage &getFrame(uint index);
+
     /// adds an image as the last frame of our animation.
     void pushFrame(QImage frame);
 
     /// adds an image to our frame at `position`
-    void insertFrame(QImage frame, uint position);
+    void insertFrame(QImage frame, uint index);
 
     /// deletes the frame at index `position`
-    void deleteFrame(uint position);
+    void deleteFrame(uint index);
 
     /// deletes the last frame of the animation
     void popFrame();
