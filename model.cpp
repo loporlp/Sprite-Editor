a4f2b0e8459b8a589be828ad2ebb728b5c162318
David Cosby <davjcosby@gmail.com>
2023-11-08 12:01:21 -0700

    encapsulated canvas specific settings into CanvasSettings and frame specific data into Frames class

--- model.cpp
+++ model.cpp
@@ -3,36 +3,60 @@
 
 Model::Model(QObject *parent)
     : QObject{parent}
+    , frames{Model::Frames()}
+    , canvasSettings{Model::CanvasSettings()}
 {}
 
-uint Model::numFrames()
+//-----Model::Frames-----//
+uint Model::Frames::numFrames()
 {
     return frames.size();
 }
 
-QImage &Model::getFrame(uint index)
+QImage &Model::Frames::getFrame(uint index)
 {
     assert(frames.size() > index);
     return frames.at(index);
 }
 
-void Model::pushFrame(QImage frame)
+void Model::Frames::pushFrame(QImage frame)
 {
     frames.push_back(frame);
 }
 
-void Model::insertFrame(QImage frame, uint index)
+void Model::Frames::insertFrame(QImage frame, uint index)
 {
     frames.insert(frames.begin() + index, frame);
 }
 
-void Model::deleteFrame(uint index)
+void Model::Frames::deleteFrame(uint index)
 {
     assert(frames.size() > index);
     frames.erase(frames.begin() + index);
 }
 
-void Model::popFrame()
+void Model::Frames::popFrame()
 {
     frames.pop_back();
+}
+
+//-----Model::CanvasSettings-----//
+const QVector2D &Model::CanvasSettings::getCanvasPosition()
+{
+    return canvasPosition;
+}
+
+void Model::CanvasSettings::setCanvasPosition(QVector2D newPosition)
+{
+    canvasPosition = newPosition;
+}
+
+float Model::CanvasSettings::getCanvasZoom()
+{
+    return canvasZoom;
+}
+
+void Model::CanvasSettings::setCanvasZoom(float newZoom)
+{
+    canvasZoom = newZoom;
 }
--- model.h
+++ model.h
@@ -3,31 +3,70 @@
 
 #include <QImage>
 #include <QObject>
+#include <QVector2D>
 
 class Model : public QObject
 {
-    std::vector<QImage> frames;
+public:
+    class Frames
+    {
+        std::vector<QImage> frames;
+
+    public:
+        /// returns the number of frames in our current animation
+        uint numFrames();
+
+        /// returns a reference to the frame at index
+        QImage &getFrame(uint index);
+
+        /// adds an image as the last frame of our animation.
+        void pushFrame(QImage frame);
+
+        /// adds an image to our frame at `position`
+        void insertFrame(QImage frame, uint index);
+
+        /// deletes the frame at index `position`
+        void deleteFrame(uint index);
+
+        /// deletes the last frame of the animation
+        void popFrame();
+    };
+
+    class CanvasSettings
+    {
+        /// Offset position of the canvas, to be modified by panning.
+        /// Stored in model so it can be serialized into our project file.
+        QVector2D canvasPosition;
+
+        /// Zoom factor of the canvas. Stored in model so it can be
+        /// serialized into our project file.
+        float canvasZoom;
+
+    public:
+        /// returns a const reference to our canvas offset position.
+        const QVector2D &getCanvasPosition();
+
+        /// updates the `canvasPosition` to the provided `newPosition`
+        void setCanvasPosition(QVector2D newPosition);
+
+        /// returns the zoom factor of our canvas
+        float getCanvasZoom();
+
+        /// updates the `canvasZoom` to the provided `newZoom`
+        void setCanvasZoom(float newZoom);
+    };
+
+private:
+    Frames frames;
+    CanvasSettings canvasSettings;
+
     Q_OBJECT
 public:
     explicit Model(QObject *parent = nullptr);
 
-    /// returns the number of frames in our current animation
-    uint numFrames();
+    Frames &getFrames();
+    CanvasSettings &getCanvasSettings();
 
-    /// returns a reference to the frame at index
-    QImage &getFrame(uint index);
-
-    /// adds an image as the last frame of our animation.
-    void pushFrame(QImage frame);
-
-    /// adds an image to our frame at `position`
-    void insertFrame(QImage frame, uint index);
-
-    /// deletes the frame at index `position`
-    void deleteFrame(uint index);
-
-    /// deletes the last frame of the animation
-    void popFrame();
 signals:
 };
 
