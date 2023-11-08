590b3e3fea736d5966d0ac1ac43f49559daf1c66
Mason Sansom <33166896+loporlp@users.noreply.github.com>
2023-11-08 11:35:06 -0700

    Displayed Image in QFrame

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,18 +3,31 @@
 #include <QPushButton>
 #include <QImage>
 #include <QLayout>
+#include <QObject>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPushButton *btn = new QPushButton();
+
+    QImage *image = new QImage();
+    //Replace with test image to see, I didn't want to add images to repo
+    //image->load("C:\\Users\\mason\\OneDrive\\Pictures\\Screenshots\\Screenshot 2023-03-01 231609.png");
+
+    QLabel *imageHolder = new QLabel("");
+    imageHolder->setPixmap(QPixmap::fromImage(image->scaled(100, 100, Qt::KeepAspectRatio)));
     auto layout = new QVBoxLayout();
-    layout->addWidget(btn);
+    layout->addWidget(imageHolder);
     ui->canvas->setLayout(layout);
 
     ui->canvas->setLineWidth(3);
+    ui->canvas->setStyleSheet("QFrame {background-color: rgb(200, 255, 255);"
+                              "border-width: 1;"
+                              "border-radius: 3;"
+                              "border-style: solid;"
+                              "border-color: rgb(10, 10, 10)}");
 
 }
 
